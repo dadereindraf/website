fbe7c668481e51382fed08b6003e49a650ecff8c
dadereindraf <dadereindraf@gmail.com.com>
2024-05-25 23:37:38 +0700

    mengubah total isi file tfidf.p dengan mengganti tanpa library apapun untuk tfidf dan naive bayes

--- tfidf.py
+++ tfidf.py
@@ -1,143 +1,223 @@
-import os
-import glob
-import re
-import math
-import nltk
-import string
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 from collections import defaultdict
 import math
-import pandas as pd
-import csv
 
-data=pd.read_csv('labeled_data.csv',encoding = 'utf8')
-tweet = data['tweet']
+# Koneksi ke database
+DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/ta'
+engine = create_engine(DATABASE_URI)
 
-# dictionary into a list
-dict=pd.read_csv('hatebase_dict.csv', encoding = 'ISO-8859-1')
-dict2 = dict['dic']
-dic = []
-for row in dict2:
-    row = row.strip("',")
-    dic.append(row)
-print(dic)
+# Ambil data latih dari database
+df_train = pd.read_sql('SELECT * FROM datalatihbungtowel', con=engine)
 
+# Ambil data uji dari database
+df_test = pd.read_sql('SELECT * FROM dataujibungtowel', con=engine)
 
-# Regular expression
-def preprocess(text):
-    text = text.strip().strip('"')
-    text = re.sub(r'[^A-Za-z0-9(),!?\.\'\`]', ' ', text)
-    text = re.sub(r"\'s", " \'s", text)
-    text = re.sub(r"\'ve", " \'ve", text)
-    text = re.sub(r"n\'t", " n\'t", text)
-    text = re.sub(r"\'re", " \'re", text)
-    text = re.sub(r"\'d", " \'d", text)
-    text = re.sub(r"\'ll", " \'ll", text)
-    text = re.sub(r",", " , ", text)
-    text = re.sub(r"\.", "  \. ", text)
-    text = re.sub(r"\"", " , ", text)
-    text = re.sub(r"!", " ! ", text)
-    text = re.sub(r"\(", " \( ", text)
-    text = re.sub(r"\)", " \) ", text)
-    text = re.sub(r"\?", " \? ", text)
-    #text = re.sub(r"\S{2,}", " ", text)
-    return text.strip().lower()
+# Tokenisasi teks
+def tokenize(text):
+    return text.lower().split()
 
-# calculate term frequency
-def phrase_frequency(text):
-    text = preprocess(text)
-    tokens = nltk.word_tokenize(text)
-    phrase1 = []
+# Menghitung Term Frequency (TF)
+def compute_tf(text):
+    tf_dict = defaultdict(int)
+    tokens = tokenize(text)
     for token in tokens:
-        if not token.isalnum():
-            continue
-        else:
-            phrase1.append(token)
-    phrases = []
-    for i in range(0,len(phrase1)-1):
-        word=phrase1[i]+" "+phrase1[i+1]
-        phrases.append(word)
-    term_freqs = {}
-    for phrase in phrases:
-        if phrase in term_freqs:
-            term_freqs[phrase] += 1 / len(phrases)
-        else:
-            term_freqs[phrase] = 1 / len(phrases)
-    return term_freqs
+        tf_dict[token] += 1
+    for token in tf_dict:
+        tf_dict[token] /= len(tokens)
+    return tf_dict
 
+# Menghitung Inverse Document Frequency (IDF)
+def compute_idf(documents):
+    idf_dict = defaultdict(int)
+    N = len(documents)
+    for document in documents:
+        tokens = set(tokenize(document))
+        for token in tokens:
+            idf_dict[token] += 1
+    for token in idf_dict:
+        idf_dict[token] = math.log(N / idf_dict[token])
+    return idf_dict
 
-def term_frequency(text):
-    text = preprocess(text)
-    tokens = nltk.word_tokenize(text)
-    term_freqs = {}
-    for token in tokens:
-        if not token.isalnum():
-            # filter out tokens that are not-alphanumeric, eg punctuation
-            continue
-        if token in term_freqs:
-            term_freqs[token] += 1 / len(text)
-        else:
-            term_freqs[token] = 1 / len(text)
-    return term_freqs
+# Menghitung TF-IDF untuk satu dokumen
+def compute_tfidf(tf, idf):
+    tfidf = {}
+    for token, tf_val in tf.items():
+        tfidf[token] = tf_val * idf.get(token, 0)
+    return tfidf
 
-def term_fre(text):
-    term_freqs = {}
-    for word in term_frequency(text).keys():
-        if word in dic:
-            term_freqs[word] = term_frequency(text)[word]
-        else:
-            continue
-    return term_freqs
+# Menghitung TF-IDF untuk semua dokumen
+def compute_tfidf_for_documents(documents):
+    idf = compute_idf(documents)
+    tfidf_list = []
+    for document in documents:
+        tf = compute_tf(document)
+        tfidf = compute_tfidf(tf, idf)
+        tfidf_list.append(tfidf)
+    return tfidf_list, idf
 
+# Membuat vektor dari hasil TF-IDF
+def create_feature_matrix(tfidf_list, idf):
+    tokens = sorted(idf.keys())
+    feature_matrix = np.zeros((len(tfidf_list), len(tokens)))
+    for i, tfidf in enumerate(tfidf_list):
+        for j, token in enumerate(tokens):
+            feature_matrix[i, j] = tfidf.get(token, 0)
+    return feature_matrix, tokens
 
+# Menghitung TF-IDF untuk data latih
+tfidf_train_list, idf = compute_tfidf_for_documents(df_train['text'])
+X_train_array, tokens = create_feature_matrix(tfidf_train_list, idf)
 
-docs = {}
-for no, rows in enumerate(tweet):
-    docs[no] = term_fre(rows)
-#print (docs[2])
+# Menghitung TF-IDF untuk data uji
+tfidf_test_list, _ = compute_tfidf_for_documents(df_test['text'])
+X_test_array, _ = create_feature_matrix(tfidf_test_list, idf)
 
+# Implementasi Naive Bayes tanpa library
+class NaiveBayesClassifier:
+    def __init__(self):
+        self.class_priors = {}
+        self.feature_probs = {}
 
-# calculated document frequency
-def document_frequency(docs):
-    freqs = {}
-    list1 = []
-    for i in range(len(docs)):
-        for item in docs[i].keys():
-            list1.append(item)  
-    for term in list1:
-        if term in freqs:
-            freqs[term] += 1 / len(docs)
-        else:
-            freqs[term] = 1 / len(docs)
-    return freqs
-#print (document_frequency(docs))
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.classes = np.unique(y)
+        
+        for cls in self.classes:
+            X_cls = X[y == cls]
+            self.class_priors[cls] = X_cls.shape[0] / n_samples
+            self.feature_probs[cls] = (X_cls.sum(axis=0) + 1) / (X_cls.sum() + n_features)  # Laplace smoothing
 
-# calculate tfidf score
-def tfidf_score(docs):
-    doc_freqs = document_frequency(docs)
-    for i in range (len(docs)):
-        for term in docs[i].keys():
-            docs[i][term] = docs[i][term] * math.log(len(docs)/doc_freqs[term], 2)
-    return docs
-#print (tfidf_score(docs)[2])
+    def predict(self, X):
+        y_pred = []
+        for x in X:
+            class_probs = {}
+            for cls in self.classes:
+                prior = np.log(self.class_priors[cls])
+                conditional = np.sum(np.log(self.feature_probs[cls]) * x)
+                class_probs[cls] = prior + conditional
+            y_pred.append(max(class_probs, key=class_probs.get))
+        return np.array(y_pred)
 
+# Inisialisasi model Naive Bayes
+nb_model = NaiveBayesClassifier()
 
-score = tfidf_score(docs)
-tot_score = {}
-for i in range(len(score)):
-    tot_score[i] = sum(score[i].values())
-#print (tot_score)
+# Latih model Naive Bayes menggunakan data latih
+nb_model.fit(X_train_array, df_train['label'])
 
+# Lakukan prediksi menggunakan model Naive Bayes
+nb_y_pred = nb_model.predict(X_test_array)
 
-output = [['ID','score']]
-for i in range(len(list(tot_score))):
-    temp = []
-    temp.append(i+1)
-    temp.append(tot_score[i])
-    output.append(temp)
+# Hitung jumlah prediksi yang benar
+correct_predictions = (df_test['label'] == nb_y_pred).sum()
 
-file=open("try.csv", 'w')
-with file:
-    writer = csv.writer(file)
-    writer.writerows(output)
-file.close()
-pd.read_csv('try.csv')
+# Hitung total prediksi
+total_predictions = len(df_test)
+
+# Hitung akurasi
+manual_accuracy = correct_predictions / total_predictions
+
+print("Akurasi Naive Bayes (Manual):", manual_accuracy)
+
+# Menghitung metrik-metrik evaluasi secara manual
+def manual_classification_report(true_labels, predicted_labels):
+    # Mendefinisikan kelas yang unik
+    classes = np.unique(true_labels)
+    
+    # Membuat dictionary untuk menyimpan metrik-metrik untuk setiap kelas
+    report = {'precision': {}, 'recall': {}, 'f1-score': {}}
+    
+    # Looping melalui setiap kelas
+    for cls in classes:
+        true_positive = ((true_labels == cls) & (predicted_labels == cls)).sum()
+        false_positive = ((true_labels != cls) & (predicted_labels == cls)).sum()
+        false_negative = ((true_labels == cls) & (predicted_labels != cls)).sum()
+        
+        # Menghitung presisi
+        precision = true_positive / (true_positive + false_positive)
+        
+        # Menghitung recall
+        recall = true_positive / (true_positive + false_negative)
+        
+        # Menghitung F1-score
+        f1_score = 2 * (precision * recall) / (precision + recall)
+        
+        # Menyimpan metrik-metrik untuk kelas saat ini dalam dictionary
+        report['precision'][cls] = precision
+        report['recall'][cls] = recall
+        report['f1-score'][cls] = f1_score
+    
+    # Menghitung rata-rata presisi, recall, dan F1-score untuk semua kelas
+    avg_precision = np.mean(list(report['precision'].values()))
+    avg_recall = np.mean(list(report['recall'].values()))
+    avg_f1_score = np.mean(list(report['f1-score'].values()))
+    
+    # Menambahkan rata-rata metrik ke dalam dictionary
+    report['precision']['avg'] = avg_precision
+    report['recall']['avg'] = avg_recall
+    report['f1-score']['avg'] = avg_f1_score
+    
+    return report
+
+# Membuat laporan klasifikasi secara manual
+manual_classification_rep = manual_classification_report(df_test['label'], nb_y_pred)
+
+# Menampilkan laporan klasifikasi secara manual
+print("Laporan Klasifikasi Naive Bayes (Manual):")
+for metric in manual_classification_rep:
+    print(metric.capitalize())
+    for cls, value in manual_classification_rep[metric].items():
+        print(f"  {cls}: {value}")
+    print()
+    
+# Menghitung confusion matrix secara manual
+def manual_confusion_matrix(true_labels, predicted_labels):
+    # Mendefinisikan kelas yang unik
+    classes = np.unique(true_labels)
+    
+    # Membuat confusion matrix berukuran (n_classes, n_classes)
+    cm = np.zeros((len(classes), len(classes)), dtype=int)
+    
+    # Mengisi confusion matrix
+    for true_label, predicted_label in zip(true_labels, predicted_labels):
+        cm[true_label, predicted_label] += 1
+    
+    return cm
+
+# Membuat confusion matrix secara manual
+manual_cm = manual_confusion_matrix(df_test['label'], nb_y_pred)
+
+# Menampilkan confusion matrix secara manual
+print("Confusion Matrix Naive Bayes (Manual):")
+for i in range(manual_cm.shape[0]):
+    for j in range(manual_cm.shape[1]):
+        print(manual_cm[i, j], end='\t')
+    print()
+    
+# Menyiapkan data hasil pengujian
+test_results = {
+    'akurasi': [manual_accuracy],
+    'precision_0': [manual_classification_rep['precision'][0]],
+    'precision_1': [manual_classification_rep['precision'][1]],
+    'avg_precision': [manual_classification_rep['precision']['avg']],    
+    'recall_0': [manual_classification_rep['recall'][0]],
+    'recall_1': [manual_classification_rep['recall'][1]],
+    'avg_recall': [manual_classification_rep['recall']['avg']],
+    'f1_score_0': [manual_classification_rep['f1-score'][0]],
+    'f1_score_1': [manual_classification_rep['f1-score'][1]],
+    'avg_f1_score': [manual_classification_rep['f1-score']['avg']],
+    'confusion_matrix_00': [manual_cm[0, 0]],
+    'confusion_matrix_01': [manual_cm[0, 1]],
+    'confusion_matrix_10': [manual_cm[1, 0]],
+    'confusion_matrix_11': [manual_cm[1, 1]]
+}
+
+# Membuat DataFrame dari data hasil pengujian
+df_test_results = pd.DataFrame(test_results)
+
+# Menyimpan DataFrame ke dalam tabel pengujian di database
+table_name = 'pengujian'  # Ganti dengan nama tabel yang sesuai
+df_test_results.to_sql(table_name, con=engine, if_exists='append', index=False)
+
+print("Hasil pengujian berhasil disimpan ke dalam tabel:", table_name)
